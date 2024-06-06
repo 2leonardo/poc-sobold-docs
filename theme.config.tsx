fb1a8646493e0aa189ca3f4bd2dca4d9b0d3ab49
2leonardo <2leonardo.esposito@gmail.com>
2024-06-06 11:00:24 +0100

    build: edit config files

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 import Logo from './components/Logo/Logo';
 
 const config: DocsThemeConfig = {
+    useNextSeoProps() {
+        const { asPath } = useRouter();
+        if (asPath !== '/') {
+            return {
+                titleTemplate: '%s - SoBold Docs',
+            };
+        }
+    },
     logo: Logo,
     project: {
-        link: 'https://github.com/SoBold/kapow-docs',
+        link: 'https://github.com/SoBold/sobold-docs',
     },
-    docsRepositoryBase: 'https://github.com/SoBold/kapow-docs',
+    docsRepositoryBase: 'https://github.com/SoBold/sobold-docs',
     footer: {
-        text: 'Kapow Docs',
+        text: 'SoBold Docs',
     },
 };
 
